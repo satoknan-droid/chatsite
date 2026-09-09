99268bce35f9c01e8454e9657bd233d1853c19e1
sk <satoknan@gmail.com>
2026-09-09 20:33:36 +0900

    Create loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,20 @@
+export default function Loading() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h2>読み込み中...</h2>
+      </div>
+    </main>
+  );
+}
